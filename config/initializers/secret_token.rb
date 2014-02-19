73410a8621db4191a9371a7a1abd3e7378b6e5f9
Brian Winterling <bwinterling@yahoo.com>
2014-02-18 19:00:50 -0700

    Created new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Floatilla::Application.config.secret_key_base = '2872c6b3391124254874744ad1ab4dd66837c57f2b0826993583d2317314502139ea1bfbf2eb3f61522cc9cc281fe51770bd2b9a3d7670e7a62ec7d63a0655f8'
