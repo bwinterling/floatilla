186f244e5c2745f0f0e278e6bc77de04c29d54a9
Simon Taranto <simon.taranto@gmail.com>
2014-02-19 16:27:18 -0700

    WIP, hard code a marker on home page map

---
+++ app/assets/javascripts/map/map.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  Map.defaultLat = 39.5500;
+  Map.defaultLong = -107.3167;
+  Map.defaultZoom = 9;
+
+  Map.geojson = [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [Map.defaultLong, Map.defaultLat]
+      },
+      "properties": {
+        "title": "Shred the gnar",
+        "description": "yup",
+        "marker-color": "#fc4353",
+        "marker-size": "large",
+        "marker-symbol": "monument"
+      }
+    },
+  ];
+
+  L.mapbox.map('map', 'srtsrt32.haldb884')
+    .setView([Map.defaultLat, Map.defaultLong], Map.defaultZoom)
+    .featureLayer.setGeoJSON(Map.geojson);
+});
