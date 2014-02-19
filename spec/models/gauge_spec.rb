07be1a1b04e39736790cafe100581775393b09c2
Simon Taranto <simon.taranto@gmail.com>
2014-02-19 11:53:55 -0700

    complete guage model validations

--- spec/models/gauge_spec.rb
+++ spec/models/gauge_spec.rb
@@ -21,6 +21,16 @@ describe Gauge do
       it "does not create gauge without a long" do
         FactoryGirl.build(:gauge, :long => nil).should_not be_valid
       end
+
+      it "does not create gauge with invalid lat" do
+        FactoryGirl.build(:gauge, :lat => 200).should_not be_valid
+        FactoryGirl.build(:gauge, :lat => -200).should_not be_valid
+      end
+
+      it "does not create gauge with invalid long" do
+        FactoryGirl.build(:gauge, :long => 200).should_not be_valid
+        FactoryGirl.build(:gauge, :long => -200).should_not be_valid
+      end
     end
 
   end
