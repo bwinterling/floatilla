6d7aadef4d0b0c3c992fa4a68c8541e87f8424a0
Simon Taranto <simon.taranto@gmail.com>
2014-02-20 13:48:13 -0700

    refactor map.js fetchGauges to pass in callback

--- app/assets/javascripts/map/map.js
+++ app/assets/javascripts/map/map.js
@@ -1,18 +1,23 @@
-$(document).ready(function() {
+$(function() {
+  "use strict";
   Map.defaultLat = 39.5500;
   Map.defaultLong = -107.3167;
   Map.defaultZoom = 9;
 
-  $.ajax({
-    type: 'GET',
-    dataType: 'json',
-    url: '/api/v1/gauges',
-    success: function(data){
-      geojson = data
-      L.mapbox.map('map', 'srtsrt32.haldb884')
-        .setView([Map.defaultLat, Map.defaultLong], Map.defaultZoom)
-        .featureLayer.setGeoJSON(geojson)
-    }
-  });
+  Map.plotGauges = function(geojson) {
+    L.mapbox.map('map', 'srtsrt32.haldb884')
+      .setView([Map.defaultLat, Map.defaultLong], Map.defaultZoom)
+      .featureLayer.setGeoJSON(geojson);
+  };
 
+  Map.fetchGauges = function(callback) {
+    $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      url: '/api/v1/gauges',
+      success: callback
+    });
+  };
+
+  Map.fetchGauges(Map.plotGauges);
 });
