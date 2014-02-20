ee2a9d5474b30a930179671b56d66957a14008f5
Simon Taranto <simon.taranto@gmail.com>
2014-02-20 13:14:03 -0700

    dynamically render guages on map

--- app/assets/javascripts/map/map.js
+++ app/assets/javascripts/map/map.js
@@ -3,38 +3,16 @@ $(document).ready(function() {
   Map.defaultLong = -107.3167;
   Map.defaultZoom = 9;
 
-  Map.geojson = [
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [Map.defaultLong, Map.defaultLat]
-      },
-      "properties": {
-        "title": "Shred the gnar",
-        "description": "yup",
-        "marker-color": "#fc4353",
-        "marker-size": "large",
-        "marker-symbol": "monument"
-      }
-    },
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [Map.defaultLong + 1, Map.defaultLat - 1]
-      },
-      "properties": {
-        "title": "Shred the gnar",
-        "description": "yup",
-        "marker-color": "#fc4353",
-        "marker-size": "large",
-        "marker-symbol": "monument"
-      }
+  $.ajax({
+    type: 'GET',
+    dataType: 'json',
+    url: '/api/v1/gauges',
+    success: function(data){
+      geojson = data
+      L.mapbox.map('map', 'srtsrt32.haldb884')
+        .setView([Map.defaultLat, Map.defaultLong], Map.defaultZoom)
+        .featureLayer.setGeoJSON(geojson)
     }
-  ];
+  });
 
-  L.mapbox.map('map', 'srtsrt32.haldb884')
-    .setView([Map.defaultLat, Map.defaultLong], Map.defaultZoom)
-    .featureLayer.setGeoJSON(Map.geojson);
 });
